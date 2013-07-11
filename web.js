b026160177955752bd10c4e9e417df7050665ba6
Ubuntu <ubuntu@ip-172-31-7-163.(none)>
2013-07-11 04:26:31 +0000

    read index.html from web.js

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var data = fs.readFile('index.html');
-  response.send(new Buffer(data, "utf-8"));
+  var buffer = new Buffer("I'm a string!", "utf-8")
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
